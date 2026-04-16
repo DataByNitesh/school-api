492eb754c866dc83d1cf110edefe45982b8c4f7a
Nitesh Kadam <niteshkadam1506@gmail.com>
2026-04-16 16:16:40 +0530

    fix db init

--- config/db.js
+++ config/db.js
@@ -3,8 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createConnection(process.env.MYSQL_URL);
-
+export const db = mysql.createPool({
+  uri: process.env.MYSQL_URL,
+  connectionLimit: 5,
+});
 db.connect((err) => {
   if (err) {
     console.log("DB Error:", err);
