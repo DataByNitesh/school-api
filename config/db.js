6c3e789041342e9382b136d7ad6cf06fabd11390
Nitesh Kadam <niteshkadam1506@gmail.com>
2026-04-16 15:43:58 +0530

    Updated DB connection to use MYSQL_URL for Railway deployment

--- config/db.js
+++ config/db.js
@@ -3,12 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+export const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) {
